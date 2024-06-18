c859ed6d2e916b61f152cbb25e0321d900c948a7
brunoabreu-levio <bruno.abreu@levio.ca>
2024-06-18 14:42:42 -0400

    Added Language Models option

--- lambda/claude-invocation/src/lambda_function.py
+++ lambda/claude-invocation/src/lambda_function.py
@@ -5,7 +5,6 @@ from multipart import parse_form_data
 from io import BytesIO
 
 REGION_NAME = "ca-central-1"
-MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0"
 SYSTEM_INSTRUCTION = ("You are Claude, an AI assistant created by Anthropic, specializing in Information Technology. "
                       "Your primary role is to assist with software development and IT-related tasks. Focus strictly "
                       "on the tasks requested, such as providing detailed explanations of code, generating tests, "
@@ -14,6 +13,7 @@ SYSTEM_INSTRUCTION = ("You are Claude, an AI assistant created by Anthropic, spe
                       "be prepared to address a broad range of other IT-related inquiries with precision and expert "
                       "knowledge. Ensure that all interactions are helpful, safe, and accurately tailored to the "
                       "specific needs of software development. Your responses should be precise and informative.")
+DEFAULT_MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0"
 DEFAULT_PROMPT = "No prompt provided"
 DEFAULT_SOURCE_CODE = "No source code provided"
 HTTP_STATUS_OK = 200
@@ -44,17 +44,19 @@ def handle_request_data(event):
     content_type = headers.get('content-type')
     if content_type and CONTENT_TYPE_MULTIPART in content_type:
         form, files = parse_multipart_data(event)
+        model = form.get('model', DEFAULT_MODEL_ID)
         source_code = files.get('file', {'value': DEFAULT_SOURCE_CODE}).value
         prompt = form.get('prompt', DEFAULT_PROMPT)
     else:
         body = json.loads(event['body'])
+        model = body.get('model', DEFAULT_MODEL_ID)
         source_code = body.get('source_code', DEFAULT_SOURCE_CODE)
         prompt = body.get('prompt', DEFAULT_PROMPT)
 
-    return source_code, prompt
+    return model, source_code, prompt
 
 
-def invoke_model(source_code, prompt):
+def invoke_model(model, source_code, prompt):
     request_body = {
         "anthropic_version": "bedrock-2023-05-31",
         "max_tokens": 4096,
@@ -76,15 +78,16 @@ def invoke_model(source_code, prompt):
             }
         ],
     }
-    model_response = client.invoke_model(modelId=MODEL_ID, body=json.dumps(request_body))
+    model_response = client.invoke_model(modelId=model, body=json.dumps(request_body))
     return json.loads(model_response["body"].read())
 
 
 def lambda_handler(event, context):
     logger.info("Request: %s", event)
+    model = DEFAULT_MODEL_ID
     try:
-        source_code, prompt = handle_request_data(event)
-        model_response_body = invoke_model(source_code, prompt)
+        model, source_code, prompt = handle_request_data(event)
+        model_response_body = invoke_model(model, source_code, prompt)
         response_text = model_response_body["content"][0]["text"]
         response = {
             "statusCode": HTTP_STATUS_OK,
@@ -92,7 +95,7 @@ def lambda_handler(event, context):
             "body": json.dumps({"response": response_text}),
         }
     except Exception as e:
-        logger.error(f"ERROR: Can't invoke '{MODEL_ID}'. Reason: {e}")
+        logger.error(f"ERROR: Can't invoke '{model}'. Reason: {e}")
         response = {
             "statusCode": HTTP_STATUS_INTERNAL_ERROR,
             "headers": {"Access-Control-Allow-Origin": "*"},
